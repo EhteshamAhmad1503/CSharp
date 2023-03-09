06fe536f5a82fd6b1c1bceb2587578628eaef982
EhteshamAhmad1503 <106243479+EhteshamAhmad1503@users.noreply.github.com>
2023-03-09 17:42:03 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Vehicle
+    {
+        int wheels;
+        Vehicle()
+        {
+            wheels = 4;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Vehicle car = new Vehicle();
+                Console.Write(car.wheels);
+
+            }
+        }
+    }
+}
+
